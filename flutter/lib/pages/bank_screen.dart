a83993197e3c7cab463454574f1fc04ac31c1200
DORA_XIE <joy8963259@gmail.com>
2024-08-24 18:22:28 +0900

    chore: update

--- flutter/lib/pages/bank_screen.dart
+++ flutter/lib/pages/bank_screen.dart
@@ -9,130 +9,132 @@ class BankScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const ProofScreen(
-                title: '',
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ProofScreen(
+                  title: '',
+                ),
               ),
+            );
+          },
+          child: Center(
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/note.png',
+                  width: 150, // or BoxFit.contain, BoxFit.fill, etc.
+                ),
+                Text(
+                  "TRANSFER TO BANK",
+                  style: GoogleFonts.delaGothicOne().copyWith(
+                    fontSize: 24,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Text(
+                      "0000-1234-5678-0000",
+                      style: GoogleFonts.delaGothicOne().copyWith(
+                        fontSize: 60,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Text(
+                      "¥112,500",
+                      style: GoogleFonts.delaGothicOne().copyWith(
+                        fontSize: 100,
+                        color: const Color(
+                            0xFFb52b12), // You can start with a large font size
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Text(
+                      "Send the exact amount to the specified bank account.",
+                      style: GoogleFonts.delaGothicOne().copyWith(
+                        fontSize: 100, // You can start with a large font size
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Text(
+                      "Double check you are sending to the correct bank account.",
+                      style: GoogleFonts.delaGothicOne().copyWith(
+                        fontSize: 100, // You can start with a large font size
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                ClipOval(
+                  child: Container(
+                    width: 160.0, // Set the width of the circle
+                    height: 160.0, // Set the height of the circle
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFb52b12), // Background color of the stamp
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      // Center the content inside the circle
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize
+                            .min, // Make the column as small as possible
+                        children: [
+                          Text(
+                            "支払",
+                            style: GoogleFonts.delaGothicOne().copyWith(
+                              fontSize: 60, // Increase the font size
+                              color: Colors.white,
+                              height:
+                                  0.8, // Adjust the height to bring lines closer together
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            "完了",
+                            style: GoogleFonts.delaGothicOne().copyWith(
+                              fontSize: 60, // Increase the font size
+                              color: Colors.white,
+                              height:
+                                  0.8, // Adjust the height to bring lines closer together
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 12),
+              ],
             ),
-          );
-        },
-        child: Center(
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/note.png',
-                width: 150, // or BoxFit.contain, BoxFit.fill, etc.
-              ),
-              Text(
-                "TRANSFER TO BANK",
-                style: GoogleFonts.delaGothicOne().copyWith(
-                  fontSize: 24,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                child: FittedBox(
-                  fit: BoxFit.scaleDown,
-                  child: Text(
-                    "0000-1234-5678-0000",
-                    style: GoogleFonts.delaGothicOne().copyWith(
-                      fontSize: 60,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                child: FittedBox(
-                  fit: BoxFit.scaleDown,
-                  child: Text(
-                    "¥112,500",
-                    style: GoogleFonts.delaGothicOne().copyWith(
-                      fontSize: 100,
-                      color: Color(
-                          0xFFb52b12), // You can start with a large font size
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                child: FittedBox(
-                  fit: BoxFit.scaleDown,
-                  child: Text(
-                    "Send the exact amount to the specified bank account.",
-                    style: GoogleFonts.delaGothicOne().copyWith(
-                      fontSize: 100, // You can start with a large font size
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                child: FittedBox(
-                  fit: BoxFit.scaleDown,
-                  child: Text(
-                    "Double check you are sending to the correct bank account.",
-                    style: GoogleFonts.delaGothicOne().copyWith(
-                      fontSize: 100, // You can start with a large font size
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              Spacer(),
-              ClipOval(
-                child: Container(
-                  width: 160.0, // Set the width of the circle
-                  height: 160.0, // Set the height of the circle
-                  decoration: BoxDecoration(
-                    color: Color(0xFFb52b12), // Background color of the stamp
-                    shape: BoxShape.circle,
-                  ),
-                  child: Center(
-                    // Center the content inside the circle
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisSize: MainAxisSize
-                          .min, // Make the column as small as possible
-                      children: [
-                        Text(
-                          "支払",
-                          style: GoogleFonts.delaGothicOne().copyWith(
-                            fontSize: 60, // Increase the font size
-                            color: Colors.white,
-                            height:
-                                0.8, // Adjust the height to bring lines closer together
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          "完了",
-                          style: GoogleFonts.delaGothicOne().copyWith(
-                            fontSize: 60, // Increase the font size
-                            color: Colors.white,
-                            height:
-                                0.8, // Adjust the height to bring lines closer together
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 12),
-            ],
           ),
         ),
       ),
