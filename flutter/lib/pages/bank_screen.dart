6d537ccd7e44d6c071a8f329cabe77815793b4c6
Pippen Wu <pipwu101@gmail.com>
2024-08-24 17:15:37 +0800

    bank screen

--- flutter/lib/pages/bank_screen.dart
+++ flutter/lib/pages/bank_screen.dart
@@ -19,12 +19,118 @@ class BankScreen extends ConsumerWidget {
           );
         },
         child: Center(
-          child: Text(
-            "支払完了",
-            style: GoogleFonts.delaGothicOne().copyWith(
-              fontSize: 150,
-            ),
-            textAlign: TextAlign.center,
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/note.png',
+                width: 150, // or BoxFit.contain, BoxFit.fill, etc.
+              ),
+              Text(
+                "TRANSFER TO BANK",
+                style: GoogleFonts.delaGothicOne().copyWith(
+                  fontSize: 24,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "0000-1234-5678-0000",
+                    style: GoogleFonts.delaGothicOne().copyWith(
+                      fontSize: 60,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "¥112,500",
+                    style: GoogleFonts.delaGothicOne().copyWith(
+                      fontSize: 100,
+                      color: Color(
+                          0xFFb52b12), // You can start with a large font size
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "Send the exact amount to the specified bank account.",
+                    style: GoogleFonts.delaGothicOne().copyWith(
+                      fontSize: 100, // You can start with a large font size
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "Double check you are sending to the correct bank account.",
+                    style: GoogleFonts.delaGothicOne().copyWith(
+                      fontSize: 100, // You can start with a large font size
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Spacer(),
+              ClipOval(
+                child: Container(
+                  width: 160.0, // Set the width of the circle
+                  height: 160.0, // Set the height of the circle
+                  decoration: BoxDecoration(
+                    color: Color(0xFFb52b12), // Background color of the stamp
+                    shape: BoxShape.circle,
+                  ),
+                  child: Center(
+                    // Center the content inside the circle
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize
+                          .min, // Make the column as small as possible
+                      children: [
+                        Text(
+                          "支払",
+                          style: GoogleFonts.delaGothicOne().copyWith(
+                            fontSize: 60, // Increase the font size
+                            color: Colors.white,
+                            height:
+                                0.8, // Adjust the height to bring lines closer together
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          "完了",
+                          style: GoogleFonts.delaGothicOne().copyWith(
+                            fontSize: 60, // Increase the font size
+                            color: Colors.white,
+                            height:
+                                0.8, // Adjust the height to bring lines closer together
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 12),
+            ],
           ),
         ),
       ),
